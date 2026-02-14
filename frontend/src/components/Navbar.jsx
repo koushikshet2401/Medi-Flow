944991d99cc9c5920f8d872b6b552b2261eeb702
koushikshet2401 <koushikshet2401@gmail.com>
2026-02-14 21:43:57 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {navbarStyles} from '../assets/dummyStyles';
+
+function Navbar() {
+  return (
+    <div>
+     Navbar
+    </div>
+  )
+}
+
+export default Navbar
