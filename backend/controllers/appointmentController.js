ae5974dc0ac8af0ffdd7aa9925ab692a1a7f2315
Adrich-Fernandes <adrichancyfernandes@gmail.com>
2026-02-19 17:52:00 +0530

    update appointment

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -512,7 +512,7 @@ export const updateAppointment = async (req, res) => {
       update.status = "Rescheduled";
       update.rescheduledTo = { date: body.date, time: body.time };
     }
-    const updated = await Appointment.findByIdAndUpdate(id,update,{})
+    const updated = await Appointment.findByIdAndUpdate(id,update,)
 
   } catch (error) {}
 };
