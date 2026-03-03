e58d8642f5f6bf4b421b1b626469997c85c2c1c7
Adrich-Fernandes <adrichancyfernandes@gmail.com>
2026-03-03 07:34:13 +0530

    added home doctor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import  Banner  from '../components/Banner';
 import Certification from '../components/Certification';
+import HomeDoctors from '../components/HomeDoctors';
 function Home() {
   return (
     <div>
       <Navbar/>
       <Banner/>
       <Certification />
+      < HomeDoctors/>
     </div>
   )
 }
